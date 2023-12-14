f0065ce3c32d8d8fb596a90caa468969772da02b
DoctorWho406 <lollo.guerrini@gmail.com>
2023-12-14 09:39:26 +0100

    Done Day 03 - Step 01 [Python]

--- 2023/Day03/Python/gear_ratios.py
+++ 2023/Day03/Python/gear_ratios.py
@@ -4,6 +4,7 @@ import re
 class Schema():
     def __init__(self, file_path: str) -> None:
         self.numbers = list[int]()
+        validity_regex = '[^\d\.\s]'
         with open(file_path) as input_file:
             lines = input_file.readlines()
             row_numbers = len(lines)
@@ -11,16 +12,28 @@ class Schema():
             for row in range(row_numbers):
                 matches = re.finditer("(\d+)", lines[row])
                 for match in matches:
-                    if row > 0 and re.findall('[^\d\.]', lines[row - 1][max(0, match.start() - 1):min(match.end() + 2, col_numbers)]):
+                    print(f'{row}: DIGIT {match.group()} ', end='')
+                    if row > 0:
+                        matches = re.findall(
+                            validity_regex, lines[row - 1][max(0, match.start() - 1):min(match.end() + 1, col_numbers)])
+                        if matches:
+                            self.numbers.append(int(match.group()))
+                            print(f'VALID for TOP {matches}')
+                            continue
+                    matches = re.findall(validity_regex, lines[row][max(
+                        0, match.start() - 1):min(match.end() + 1, col_numbers)])
+                    if matches:
                         self.numbers.append(int(match.group()))
+                        print(f'VALID {matches}')
                         continue
-                    if re.findall('[^\d\.]', lines[row][max(0, match.start() - 1):min(match.end() + 2, col_numbers)]):
-                        self.numbers.append(int(match.group()))
-                        continue
-                    if row < row_numbers - 1 and re.findall('[^\d\.]', lines[row + 1][max(0, match.start() - 1):min(match.end() + 2, col_numbers)]):
-                        self.numbers.append(int(match.group()))
-                        continue
-                    print(f'{row}: DIGIT {match.group()} NOT VALID')
+                    if row < row_numbers - 1:
+                        matches = re.findall(
+                            validity_regex, lines[row + 1][max(0, match.start() - 1):min(match.end() + 1, col_numbers)])
+                        if matches:
+                            self.numbers.append(int(match.group()))
+                            print(f'VALID for BOTTOM {matches}')
+                            continue
+                    print(f'NOT VALID')
 
     def get_sum(self) -> int:
         return sum(self.numbers)
@@ -29,12 +42,6 @@ class Schema():
 if __name__ == '__main__':
     # input: The relative path of input file
     # Ex: '2023\Day03\input.txt'
-    # file_path = input('Relative path of input file: ')
-    file_path = '2023\Day03\input.txt'
+    file_path = input('Relative path of input file: ')
     s = Schema(file_path)
-    # 420712    LOW
-    # 395780    LOW
-    # 531491    HIGH
-    # 538346
-    # 531491
     print(s.get_sum())
